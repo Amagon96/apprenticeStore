5d6cfa3560f93325426e2a4f40d19697797f0ed0
Gerardo Camacho <gdotcamacho@gmail.com>
2022-04-18 04:52:24 -0600

    add categories controller

--- src/main/java/com/example/demo/controllers/CategoryController.java
+++ src/main/java/com/example/demo/controllers/CategoryController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import com.example.demo.models.Categories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
+    List<Categories> categories = List.of(
+            new Categories(1L, "sports"),
+            new Categories(2L, "clothing")
+    );
+
+    public Categories getCategory(String name) {
+        Categories matchingCategory = null;
+        for (Categories category : categories) {
+            if (category.getName() == name) {
+                matchingCategory = category;
+            }
+        }
+        return matchingCategory;
+    }
+
     @GetMapping
-    public List<String> getCategories() {
-        return List.of("Hello", "World");
+    public List<Categories> getCategories() {
+        return categories;
     }
 }
